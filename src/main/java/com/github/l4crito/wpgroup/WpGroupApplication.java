c95b54c4f02588e67546de508b289f9991f4406c
Christian Caicedo <christian.caicedo@grainger.com>
2022-01-24 18:31:23 -0500

    first commit

---
+++ src/main/java/com/github/l4crito/wpgroup/WpGroupApplication.java
@@ -0,0 +1,13 @@
+package com.github.l4crito.wpgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpGroupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/l4crito/wpgroup/WpGroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.l4crito.wpgroup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpGroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
